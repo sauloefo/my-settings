c86a3f8743202be4f7d8d01e11232a77f0a9a4d7
Saulo Oliveira <me@sauloefo.com>
2022-10-23 20:20:44 +0100

    Add asdf file sourcing

--- init.fish
+++ init.fish
@@ -3,3 +3,4 @@ set -x MY_SETTINGS_PATH (status dirname)
 # . "$HOME"/.config/ssh/setup.sh
 source "$MY_SETTINGS_PATH"/aliases/init.fish
 source "$MY_SETTINGS_PATH"/omp/init.fish
+source ~/.asdf/asdf.fish
