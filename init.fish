2f7356faee9191b598303452db45566d32bca00a
Saulo Oliveira <me@sauloefo.com>
2023-03-25 09:44:04 +0000

    start ssh and add ssh key

--- init.fish
+++ init.fish
@@ -1,5 +1,7 @@
 set -x MY_SETTINGS_PATH (status dirname)
 set -U EDITOR vim
+fish_ssh_agent
+ssh-add ~/.ssh/github-private-key
 # . "$HOME"/.config/nvm/setup.sh
 # . "$HOME"/.config/ssh/setup.sh
 source "$MY_SETTINGS_PATH"/aliases/init.fish
