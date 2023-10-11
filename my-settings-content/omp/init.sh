d1d9b95a064feeb434e4ae27af5518ce8e6a0f77
Saulo Oliveira <me@saulo.pro>
2023-10-11 16:17:34 +0100

    Read omp theme from file

--- my-settings-content/omp/init.sh
+++ my-settings-content/omp/init.sh
@@ -1,1 +1,2 @@
-eval "$(oh-my-posh --init --shell bash --config /etc/oh-my-posh/themes/stelbent.minimal.omp.json)"
+script_folder=$(dirname $BASH_SOURCE)
+eval "$(oh-my-posh --init --shell bash --config /etc/oh-my-posh/themes/$(cat $script_folder/theme-name).omp.json)"
